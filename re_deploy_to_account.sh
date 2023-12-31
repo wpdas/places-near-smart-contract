db211aa498a78e442be6d23a7c522a647d397b73
Wenderson Pires <wendersonpdas@gmail.com>
2023-12-31 13:45:55 -0300

    scripts updated

--- re_deploy_to_account.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-near deploy place-contract.wendersonpires.testnet --wasmFile ./target/wasm32-unknown-unknown/release/places_near_contract.wasm
--- test.sh
+++ test.sh
@@ -2,8 +2,3 @@
 
 # unit testing
 cargo test
-
-# sandbox testing
-./build.sh
-cd sandbox-rs
-cargo run --example sandbox "../target/wasm32-unknown-unknown/release/places_near_contract.wasm"
